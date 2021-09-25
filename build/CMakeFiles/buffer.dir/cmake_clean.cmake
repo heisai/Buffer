73e562c82a5cc89236b9cacec7a018ce84482517
yang sen <1049879353@qq.com>
2021-09-25 20:16:50 +0800

    修改文档

---
+++ build/CMakeFiles/buffer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/buffer.dir/Buffer.cpp.o"
+  "CMakeFiles/buffer.dir/main.cpp.o"
+  "buffer"
+  "buffer.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/buffer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
