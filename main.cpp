c65b1dc337eb1a77be8f05e3517b904d36eac001
yang sen <1049879353@qq.com>
2021-09-25 20:17:22 +0800

    修改文档

--- main.cpp
+++ main.cpp
@@ -4,19 +4,22 @@ using namespace std;
 
 int main()
 {
+
     Buffer bf;
-    bf.PutUint8(10);
-    bf.PutUint8(11);
-    bf.PutUint16(1000);
-    bf.PutUint32(10000);
-    bf.PutString("qwertyuio");
-    bf.PutUint8(7);
-    std::cout<<int(bf.size())<<std::endl;
-    std::cout<<int(bf.ReadUint8())<<std::endl;
-    std::cout<<int(bf.ReadUint8())<<std::endl;
-    std::cout<<int(bf.ReadUint16())<<std::endl;
-    std::cout<<int(bf.ReadUint32())<<std::endl;
-    std::cout<<(bf.ReadString(9))<<std::endl;
-    std::cout<<int(bf.ReadUint8())<<std::endl;
+    bf.PutUint8(10);            // 版本号
+    bf.PutUint8(11);            // 功能
+    bf.PutUint16(3);            //模式
+    bf.PutUint32(9);            //数据长度
+    bf.PutString("qwertyuio");  //数据
+    bf.PutUint8(7);             //结束标志
+    std::cout<<"数据长度:"<<int(bf.size())<<std::endl;
+
+    std::cout<<"版本号:"<<int(bf.ReadUint8())<<std::endl;
+    std::cout<<"功能:"<<int(bf.ReadUint8())<<std::endl;
+    std::cout<<"模式:"<<int(bf.ReadUint16())<<std::endl;
+    int len = bf.ReadUint32();
+    std::cout<<"数据长度:"<<len<<std::endl;
+    std::cout<<"数据:"<<(bf.ReadString(len))<<std::endl;
+    std::cout<<"结束标志:"<<int(bf.ReadUint8())<<std::endl;
    return 0;
 }
